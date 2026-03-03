5a09a85228667997c6816284191ad510f4166b41
xplexer-dev <xplexer-dev@proton.me>
2026-03-03 17:57:26 +0100

    paly with schema

--- internal/domain/project_test.go
+++ internal/domain/project_test.go
@@ -1,11 +1,14 @@
 package domain_test
 
 import (
+	"encoding/json"
+	"testing"
+
 	"github.com/google/jsonschema-go/jsonschema"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/xplexer-lab/xplexer/internal/domain"
 	"github.com/xplexer-lab/xplexer/pkg/xkit/errpack"
-	"testing"
 )
 
 func TestProject_CreateSchema(t *testing.T) {
@@ -23,5 +26,54 @@ func TestProject_CreateSchema(t *testing.T) {
 		var perr errpack.Error
 		require.ErrorAs(t, err, &perr)
 		require.True(t, perr.Type() == errpack.TypeDomain)
+	})
+
+	t.Run("try validation against schema", func(t *testing.T) {
+		t.Skipf("just temporary impl")
+
+		type User struct {
+			Name string `json:"name"`
+			Age  int    `json:"age"`
+		}
+
+		schema := jsonschema.Schema{}
+		schema.Properties = make(map[string]*jsonschema.Schema)
+		schema.Types = []string{"object"}
+
+		name := &jsonschema.Schema{}
+		name.Default = json.RawMessage(`"don"`)
+		name.Types = []string{"string", "null"}
+
+		age := &jsonschema.Schema{}
+		age.Minimum = jsonschema.Ptr(1.0)
+		age.Maximum = jsonschema.Ptr(100.0)
+		age.Types = []string{"integer"}
+
+		schema.Properties["name"] = name
+		schema.Properties["age"] = age
+		schema.Required = []string{"age", "name"}
+
+		jData, err := schema.MarshalJSON()
+		assert.NoError(t, err)
+		t.Logf("%s", string(jData))
+
+		resolved, err := schema.Resolve(nil)
+		assert.NoError(t, err)
+		user := User{
+			Name: "",
+			Age:  101,
+		}
+
+		uData, err := json.Marshal(user)
+		assert.NoError(t, err)
+
+		var input any
+		err = json.Unmarshal(uData, &input)
+		assert.NoError(t, err)
+
+		err = resolved.Validate(input)
+		assert.NoError(t, err)
+
+		t.Logf("udata=%s", string(uData))
 	})
 }
